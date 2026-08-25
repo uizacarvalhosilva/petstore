6ac6926dab11d7a355702be07cbb327ad40749c2
Uíza <uizacarvalhosilva@gmail.com>
2026-08-25 12:05:13 -0300

    ajuste para usar o service nos testes

--- tests/pet/pet.test.js
+++ tests/pet/pet.test.js
@@ -1,25 +1,21 @@
-const axios = require('axios');
-require('dotenv').config();
+const {getPetsByStatus,getPetById,createPet,updatePet,deletePet} = require('../../services/pet.services');
 
 describe('Fluxo PETs', () => {
 
   test('Buscar todos os pets', async () => {
-    const response = await axios.get(
-      `${process.env.BASE_URL}/pet/findByStatus?status=available`
-    );
+    const response = await getPetsByStatus('available');
 
     expect(response.status).toBe(200);
     expect(Array.isArray(response.data)).toBe(true);
-    console.log('Lista de pets disponíveis:', response.data);
   });
+
   test('Buscar um pet pelo ID', async () => {
-    const response = await axios.get(
-      `${process.env.BASE_URL}/pet/3`
-    );
+    const response = await getPetById(3);
+
     expect(response.status).toBe(200);
     expect(response.data).toHaveProperty('id');
-    console.log('Detalhes do pet com ID 3:', response.data);
   });
+
   test('Cadastrar um novo pet', async () => {
     const newPet = {
       id: 123456,
@@ -27,20 +23,12 @@ describe('Fluxo PETs', () => {
       status: 'available'
     };
 
-    const response = await axios.post(
-      `${process.env.BASE_URL}/pet`,
-      newPet,
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-    );
+    const response = await createPet(newPet);
 
     expect(response.status).toBe(200);
     expect(response.data).toHaveProperty('id', newPet.id);
-    console.log('Novo pet cadastrado:', response.data);
   });
+
   test('Atualizar um pet existente', async () => {
     const updatedPet = {
       id: 123456,
@@ -48,28 +36,16 @@ describe('Fluxo PETs', () => {
       status: 'sold'
     };
 
-    const response = await axios.put(
-      `${process.env.BASE_URL}/pet`,
-      updatedPet,
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-    );
+    const response = await updatePet(updatedPet);
 
     expect(response.status).toBe(200);
     expect(response.data).toHaveProperty('name', updatedPet.name);
-    console.log('Pet atualizado:', response.data);
   });
-  test('Deletar um pet pelo ID', async () => {
-    const petIdToDelete = 123456;
 
-    const response = await axios.delete(
-      `${process.env.BASE_URL}/pet/${petIdToDelete}`
-    );
+  test('Deletar um pet pelo ID', async () => {
+    const response = await deletePet(123456);
 
     expect(response.status).toBe(200);
-    console.log(`Pet com ID ${petIdToDelete} deletado com sucesso.`);
   });
+
 });
